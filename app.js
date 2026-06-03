8d29b154dfe86c63564941e1dc9ca6a5a1ddb2d9
mgc90 <matiascorrea@protonmail.com>
2026-06-02 23:44:52 -0300

    concurrently instalado y corriendo en script dev junto con tailwind y nodemon funcionando correctamente.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { connectDatabase } from './models/index.js';
 import express from 'express';
 var app = express();
 
+const PORT = process.env.PORT;
 
 
 
@@ -23,12 +24,12 @@ connectDatabase()
   .then(() => {
     app.listen(PORT, (err) => {
       if(err) {
-        console.error('[+] Error al iniciar el servidor:', err);
+        console.error('Error al iniciar el servidor:', err);
         return;
       }
-      console.log(`[+] Servidor escuchando en el puerto ${PORT}`);
+      console.log(`Servidor activo en el puerto ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('[+] Error sincronizando con bd:', err)
+    console.error('Error sincronizando con bd:', err)
   })
---
+++ models/User.js
@@ -0,0 +1,75 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "./config.js";
+import bcrypt from 'bcrypt';
+
+export class User extends Model {
+  // Metodo para verificar contrasena
+  validatePassword(password){
+    return bcrypt.compare(password, this.password)
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING, //255
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: 'Debe ser un email!'
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    birthDate: {
+      type: DataTypes.DATEONLY,
+    },
+    avatar: {
+      type: DataTypes.BLOB,
+    },
+    password: {
+      type: DataTypes.STRING,
+    }
+  },
+  {
+    sequelize, // necesario para conectarse a la bd
+    modelName: 'User', // nombre del modelo
+    tableName: 'users', // nombre de la tabla
+    createdAt: true, // cada vez que crea un usuario coloca la fecha de creacion
+    deletedAt: true, // cada vez que se elimina un usuario coloca la fecha de eliminacion
+    hooks: {
+      beforeSave: async (usuario) => {
+        if(!usuario.password) return;
+        if(!usuario.changed('password')) return;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(usuario.password, salt)
+        usuario.password = hashedPassword;
+      }
+    }
+  },
+);
+
+// user 
+// id not null autoincremental
+// firstname not null varchar(50)
+// lastname not null varchar(50)
+// birthDate Date
+// email not null varchar
+// phone varchar
+// -- auditoria
+// createdAt
+// deletedAt => null | Date para si está activo o no. si es null usuario
